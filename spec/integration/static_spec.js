9cbb2af7d7346fba94063312952c677cf9709ba9
Claire Ouellette <claireelisey@gmail.com>
2018-10-09 17:24:11 -0500

    Checkpoint Routing, Controllers, and View: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,10 +6,10 @@ const marcoBase = "http://localhost:3000/marco";
 describe("routes : static", () => {
 
     describe("GET /", () => {
-    it("should return status code 200", (done) => {
-
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
         request.get(base, (err, res, body) => {
             expect(res.statusCode).toBe(200);
+            expect(body).toContain("Welcome to Bloccit");
 
             done();
         });
