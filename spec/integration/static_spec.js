268df5d3a501bfa04f089c6235c497f06ff9cd12
Claire Ouellette <claireelisey@gmail.com>
2018-10-09 19:02:05 -0500

    Assignment Routing, Controllers, and View: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,16 +6,16 @@ const marcoBase = "http://localhost:3000/marco";
 describe("routes : static", () => {
 
     describe("GET /", () => {
-        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
-        request.get(base, (err, res, body) => {
-            expect(res.statusCode).toBe(200);
-            expect(body).toContain("Welcome to Bloccit");
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain("Welcome to Bloccit");
 
-            done();
+                done();
+            });
         });
-    });
 
-  });
+    });
 
     describe("GET /marco", () => {
         it("should return status code 200", (done) => {
